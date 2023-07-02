3add86de93e8ae28a77f6ebb6c5360066cfff61d
Yahya Mahmud Orhan <e1327280@student.tuwien.ac.at>
2023-07-02 18:23:19 +0200

    connected backend and frontend

--- PassMe/src/main/java/com/passmeindustries/PassMe/Controller/Controller.java
+++ PassMe/src/main/java/com/passmeindustries/PassMe/Controller/Controller.java
@@ -1,12 +1,14 @@
 package com.passmeindustries.PassMe.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class Controller {
     @GetMapping("/getresponse")
     public String getResponse(){
-        return "I'm alive!";
+        return "I am alive";
     }
 }
--- pass-me-frontend/src/App.js
+++ pass-me-frontend/src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // GET request
+    fetch('http://localhost:8080/getresponse')
+      .then(response => {
+        return response.text()
+      })
+      .then(text => {
+        console.log(text)
+      })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 
         </p>
         <a
           className="App-link"
